24a702dc508295ed2b5ad69caf90c864eded0ffb
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-03-19 11:39:16 +0300

    Starting reactive form

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { sign_up } from '../../Models/sign-up';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class YourComponent {
+  signUpForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+      this.signUpForm = this.fb.group({
+          userName: ['', [Validators.required]],
+          email: ['', [Validators.required]],
+          password: ['', [Validators.required,]],
+          confirmPassword: ['', [Validators.required,]],
+      });
+  }
 }
---
+++ src/app/Models/sign-up.ts
@@ -0,0 +1,6 @@
+export class sign_up{
+    "username":string;
+    "email":string;
+    "password":string;
+    "confirmPassword":string;
+}
