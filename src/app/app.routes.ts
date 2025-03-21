b9233faa9246d7e8ee017713edc927952198260f
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-03-21 10:11:45 +0300

    Renaming service folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { HomesComponent } from './Components/homes/homes.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
-import { ServicesComponent } from './Components/services/services.component';
+import { ServicesComponent } from './Components/services-componenet-folder/services.component';
 
 export const routes: Routes = [
     {
