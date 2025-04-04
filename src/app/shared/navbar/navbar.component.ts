53e124928662162327a2783044c4362c2eee3293
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-04-04 10:16:29 +0300

    Navbar first look + routing in components

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @Input() page!: string;
 
 }
