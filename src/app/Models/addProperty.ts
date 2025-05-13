00718f52a3b78f9f7a3fbccebe4578cfbbb101ca
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-05-13 20:18:47 +0300

    add property model + property service

---
+++ src/app/Models/addProperty.ts
@@ -0,0 +1,24 @@
+export class addProperty {
+  'country_id': number;
+  'city_id': number;
+  'latitude': number;
+  'longitude': number;
+  'additional_info': number;
+  'exposure': [];
+  'area': number;
+  'bathrooms': number;
+  'balconies': number;
+  'amenities': [];
+  'images': [];
+  'physical_status_type_id': number;
+  'is_furnished': number;
+  'property_type_id': number;
+  'floor': number;
+  'building_number': number;
+  'apartment_number': number;
+  'commercial_property_type_id': number;
+  'sell_type_id': number;
+  'price': number;
+  'lease_period': string;
+  'payment_plan': string;
+}
---
+++ src/app/Services/property.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../Models/ApiResponse';
+import { environment } from '../shared/environments';
+import { propertyCard } from '../Models/property-card';
+import { PaginatedData } from '../Models/paginatedData';
+import { addProperty } from '../Models/addProperty';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PropertyService {
+  constructor(private http: HttpClient) {}
+
+  public viewProperty(
+    readyOrOff: 'ready-to-move-in',
+    comOrRes: 'commercial',
+    purOrRent: string
+  ): Observable<ApiResponse<PaginatedData<propertyCard>>> {
+    let url = `${environment.Api}/view-properties/${readyOrOff}/${comOrRes}/${purOrRent}`;
+    return this.http.get<ApiResponse<PaginatedData<propertyCard>>>(url);
+  }
+  public addProperty(property: addProperty): Observable<ApiResponse<null>> {
+    let url = `${environment.Api}/add-property`;
+    return this.http.post<ApiResponse<null>>(url, property);
+  }
+}
