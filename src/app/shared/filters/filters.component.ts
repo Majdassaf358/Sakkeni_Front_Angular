b39f9ead7372c0188532a8e47d7dc8e18af716e7
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-05-22 09:53:05 +0300

    Side filter color select

--- src/app/shared/filters/filters.component.ts
+++ src/app/shared/filters/filters.component.ts
@@ -1,17 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent {
   @Input() page!: string;
   @Output() filterChange = new EventEmitter<string>();
-
+  sideSelect: string = 'list;';
   changeDisplay(change: string) {
     this.filterChange.emit(change);
+    this.sideSelect = change;
   }
 }
