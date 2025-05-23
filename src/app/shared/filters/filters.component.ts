d2c602c7d24a355361a78c7b479ab6e1c2d94901
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-05-23 17:31:58 +0300

    Fixing sideSelect default name

--- src/app/shared/filters/filters.component.ts
+++ src/app/shared/filters/filters.component.ts
@@ -11,7 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class FiltersComponent {
   @Input() page!: string;
   @Output() filterChange = new EventEmitter<string>();
-  sideSelect: string = 'list;';
+  sideSelect: string = 'list';
   changeDisplay(change: string) {
     this.filterChange.emit(change);
     this.sideSelect = change;
