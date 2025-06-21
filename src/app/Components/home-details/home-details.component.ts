d722bcc6890d7c11f4bcd3456199e24f800bd0ba
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-06-21 08:42:17 +0300

    Home-details slider Done

--- src/app/Components/home-details/home-details.component.ts
+++ src/app/Components/home-details/home-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { FiltersComponent } from '../../shared/filters/filters.component';
 import { ActivatedRoute, RouterModule } from '@angular/router';
@@ -17,6 +17,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './home-details.component.css',
 })
 export class HomeDetailsComponent implements OnInit {
+  @ViewChild('slider', { static: true }) scrollContainer!: ElementRef;
+
+  isDragging = false;
+  startX = 0;
+  scrollLeft = 0;
+
   homeIdSRT: string = '';
   homeId: number = 0;
   images: string[] = [];
@@ -47,8 +53,23 @@ export class HomeDetailsComponent implements OnInit {
       console.log(error);
     }
   }
-  openGallery() {
-    // You can open a modal/lightbox or route to another page
-    console.log('Open full gallery');
+  startDrag(event: MouseEvent): void {
+    this.isDragging = true;
+    this.scrollContainer.nativeElement.classList.add('dragging');
+    this.startX = event.pageX - this.scrollContainer.nativeElement.offsetLeft;
+    this.scrollLeft = this.scrollContainer.nativeElement.scrollLeft;
+  }
+
+  stopDrag(): void {
+    this.isDragging = false;
+    this.scrollContainer.nativeElement.classList.remove('dragging');
+  }
+
+  onDrag(event: MouseEvent): void {
+    if (!this.isDragging) return;
+    event.preventDefault();
+    const x = event.pageX - this.scrollContainer.nativeElement.offsetLeft;
+    const walk = (x - this.startX) * 2; // Adjust speed here
+    this.scrollContainer.nativeElement.scrollLeft = this.scrollLeft - walk;
   }
 }
