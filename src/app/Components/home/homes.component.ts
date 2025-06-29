873fc774812a449a4dacb6c6efc6dffb99fa47f5
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-06-29 14:00:59 +0300

    Home images position is right

--- src/app/Components/home/homes.component.ts
+++ src/app/Components/home/homes.component.ts
@@ -12,17 +12,32 @@ import { FiltersComponent } from '../../shared/filters/filters.component';
   styleUrls: ['./homes.component.css'],
 })
 export class HomesComponent implements OnInit {
-  images: string[] = [
-    'assets/Imgs/landing1.jpg',
-    'assets/Imgs/landing2.jpg',
-    'assets/Imgs/landing3.jpg',
+  slides = [
+    {
+      image: 'assets/Imgs/landing1.jpg',
+      title: 'One Platform,<br />Infinite Property Possibilities',
+      text: 'Buy, sell or rent, your next move,<br /> starts here!',
+      position: 'center 78%',
+    },
+    {
+      image: 'assets/Imgs/landing2.jpg',
+      title: 'Everything You Need<br /> Under One Roof',
+      text: 'From construction to careers, bring your vision or<br /> find your next role.',
+      position: 'center 13%',
+    },
+    {
+      image: 'assets/Imgs/landing3.jpg',
+      title: 'From Start To Keys<br /> in Your Hand!',
+      text: 'Because finding a home<br /> should be this easy.',
+      position: 'center 51%',
+    },
   ];
   currentImage = 0;
   private swapInterval!: number;
   constructor() {}
   ngOnInit(): void {
     this.swapInterval = window.setInterval(() => {
-      this.currentImage = (this.currentImage + 1) % this.images.length;
+      this.currentImage = (this.currentImage + 1) % this.slides.length;
     }, 5000);
   }
   ngOnDestroy(): void {
