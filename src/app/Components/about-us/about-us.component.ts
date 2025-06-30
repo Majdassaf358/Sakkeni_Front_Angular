43dc0025c2803c302ee481ddd1799b6902fb8d27
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-06-30 10:45:51 +0300

    Google map works in about us

--- src/app/Components/about-us/about-us.component.ts
+++ src/app/Components/about-us/about-us.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, GoogleMapsModule, CommonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  center: google.maps.LatLngLiteral = { lat: 52.3676, lng: 4.9041 };
+  markerLatLong: google.maps.LatLngLiteral[] = [
+    { lat: 52.3676, lng: 5.9041 },
+    { lat: 52.3676, lng: 6.9041 },
+  ];
+}
