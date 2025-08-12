8f17dcba205a184170f4f47972e17cc150cc1b70
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-08-12 14:35:57 +0300

    navbar good for now

--- projects/dashboard/src/app/Components/view-statistics/view-statistics.component.ts
+++ projects/dashboard/src/app/Components/view-statistics/view-statistics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../shared/side-bar/side-bar.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-view-statistics',
-  imports: [SideBarComponent],
+  imports: [SideBarComponent, NavbarComponent],
   templateUrl: './view-statistics.component.html',
   styleUrl: './view-statistics.component.css',
 })
