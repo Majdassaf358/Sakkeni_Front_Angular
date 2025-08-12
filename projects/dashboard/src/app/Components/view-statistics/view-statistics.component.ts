fca423e7f04e0de0fa4a46bf026919ca93c1a715
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-08-12 14:31:44 +0300

    Sidebar good for now

--- projects/dashboard/src/app/Components/view-statistics/view-statistics.component.ts
+++ projects/dashboard/src/app/Components/view-statistics/view-statistics.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from '../../shared/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-view-statistics',
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './view-statistics.component.html',
-  styleUrl: './view-statistics.component.css'
+  styleUrl: './view-statistics.component.css',
 })
-export class ViewStatisticsComponent {
-
-}
+export class ViewStatisticsComponent {}
