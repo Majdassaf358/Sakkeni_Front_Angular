2b82aeea9d160a7777b0ae7e4827e790783ef4b1
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-08-18 08:36:43 +0300

    adding admin register API

--- projects/dashboard/src/app/Services/auth.service.ts
+++ projects/dashboard/src/app/Services/auth.service.ts
@@ -5,6 +5,7 @@ import { ApiResponse } from '../Models/ApiResponse';
 import { AdminRes } from '../Models/AdminRes';
 import { login } from '../Models/auth/login';
 import { environment } from '../shared/environments';
+import { admin_reg } from '../Models/auth/admin_reg';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,10 @@ import { environment } from '../shared/environments';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  public adminReg(req: admin_reg): Observable<ApiResponse<AdminRes>> {
+    let url = `${environment.Api}/register-admin`;
+    return this.http.post<ApiResponse<AdminRes>>(url, req);
+  }
   public adminLogin(req: login): Observable<ApiResponse<AdminRes>> {
     let url = `${environment.Api}/admin-login`;
     return this.http.post<ApiResponse<AdminRes>>(url, req);
